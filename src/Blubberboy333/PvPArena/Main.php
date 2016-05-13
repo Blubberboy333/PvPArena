ccd3f5ad926eef4fd28d8255e1931935b1586c07
Blubberboy333 <thecowmooman@gmail.com>
2016-05-13 12:06:58 -0400

    Update Main.php

--- src/Blubberboy333/PvPArena/Main.php
+++ src/Blubberboy333/PvPArena/Main.php
@@ -1,1 +1,16 @@
 <?php
+
+namespace Blubberboy333\PvPArena;
+
+use pocketmine\event\Listener;
+use pocketmine\event\entity\EntityDeathEvent;
+use pocketmine\event\entity\EntityMotionEvent;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener{
+  
+}
